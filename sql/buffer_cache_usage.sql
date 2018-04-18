93e16fd74c3cca8375cd303ade4501c39c96e792
Marcus Gartner <marcus@lob.com>
2018-04-17 17:45:47 -0700

    added first scripts

---
+++ sql/buffer_cache_usage.sql
@@ -0,0 +1,11 @@
+SELECT c.relname
+  , pg_size_pretty(count(*) * 8192) as buffered
+  , round(100.0 * count(*) / ( SELECT setting FROM pg_settings WHERE name='shared_buffers')::integer,1) AS buffers_percent
+  , round(100.0 * count(*) * 8192 / pg_relation_size(c.oid),1) AS percent_of_relation
+ FROM pg_class c
+ INNER JOIN pg_buffercache b ON b.relfilenode = c.relfilenode
+ INNER JOIN pg_database d ON (b.reldatabase = d.oid AND d.datname = current_database())
+ WHERE pg_relation_size(c.oid) > 0
+ GROUP BY c.oid, c.relname
+ ORDER BY 3 DESC
+ LIMIT 20;
