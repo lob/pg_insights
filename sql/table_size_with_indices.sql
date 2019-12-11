7db25b519d2b3713b615b0dd9b26e1d2602650e6
Marcus Gartner <marcus@lob.com>
2019-12-10 16:50:45 -0800

    Order table_size_with_indices.sql results by total relation size

--- sql/table_size_with_indices.sql
+++ sql/table_size_with_indices.sql
@@ -5,4 +5,4 @@ LEFT JOIN pg_namespace n ON (n.oid = c.relnamespace)
 WHERE n.nspname NOT IN ('pg_catalog', 'information_schema')
   AND n.nspname !~ '^pg_toast'
   AND c.relkind='r'
-ORDER BY pg_table_size(c.oid) DESC;
+ORDER BY pg_total_relation_size(c.oid) DESC;
